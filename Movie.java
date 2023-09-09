8b5cc2dad8930d5bf1f4e9400cb234a5a3437eaf
Elena <enidelcu@gmail.com>
2023-09-09 18:34:59 +0300

    HW_1-OOP Movie class posted

---
+++ Movie.java
@@ -0,0 +1,21 @@
+public class Movie {
+
+    String title = "";
+    String director = "";
+    int rel_year = 2000;
+    int run_time = 0;
+    int buget = 0;
+    int box_office = 0;
+
+    static boolean watched = false;
+    static boolean want_to_watch = false;
+
+    public static void watch() {
+        watched = true;
+    }
+    public static void add_to_list() {
+        want_to_watch = true;
+    }
+
+
+}
